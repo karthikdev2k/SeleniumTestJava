78d2c4e290917b31aeadda0d7b33ae604e85cfe0
Karthik <Karthik.dev2k@gmail.com>
2017-11-05 21:03:31 +0530

    Initial commit - adding all files into the remote repo

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,36 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest
+{
+	private WebDriver driver;
+
+	@Test
+	public void testEasy()
+	{
+		driver.get("http://demo.guru99.com/selenium/guru99home/");
+		String title = driver.getTitle();
+		System.out.println("title = "+title);
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest()
+	{
+		System.setProperty("webdriver.gecko.driver","C:/temp/kar/geckodriver/geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+
+	@AfterTest
+	public void afterTest()
+	{
+		driver.quit();
+	}
+}
---
+++ WebdriverTest/src/test/java/example/NewTestHeadless.java
@@ -0,0 +1,48 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.interactions.HasInputDevices;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTestHeadless {
+  @Test
+	public static void main(String[] args) {
+	  
+		// Declaring and initialising the HtmlUnitWebDriver
+		HtmlUnitDriver unitDriver = new HtmlUnitDriver();
+
+		// open google.com webpage
+		unitDriver.get("http://google.com");
+
+		System.out.println("Title of the page is -> " + unitDriver.getTitle());
+
+		// find the search edit box on the google page
+		WebElement searchBox = unitDriver.findElement(By.name("q"));
+
+		// type in Selenium
+		searchBox.sendKeys("Selenium");
+
+		// find the search button
+		WebElement button = unitDriver.findElement(By.name("gbqfba"));
+
+		// Click the button
+		button.click();
+
+		System.out.println("Title of the page is -> " + unitDriver.getTitle());
+
+	}
+  
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
